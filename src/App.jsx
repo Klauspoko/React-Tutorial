061399565711c1154ecd17d180118a0471ccf11f
Divine Poko <pokodivine@gmail.com>
2024-01-03 10:35:50 +0100

    Hollywood Movie list

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,42 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="wrapper">
+        <header>
+            <div className="page-banner">
+                <h1 className="title">Hollywood Movies List</h1>
+                <p className='subhead'>All time best American Movies</p>
+                <form className="search-movies">
+                    <input type="text" placeholder="Search Movies..."/>
+                </form>
+            </div>
+        </header>
+        <div className="book-list">
+            <h2 className="title">Movies</h2>
+            <ul>
+                <li>
+                    <span className="name">Fast and Furious</span>
+                    <span className="delete">delete</span>    
+                </li>
+                <li>
+                    <span className="name">Escape Plan</span>
+                    <span className="delete">delete</span>
+                </li>
+                <li>
+                    <span className="name">Loki</span>
+                    <span className="delete">delete</span>    
+                </li>
+                <li>
+                    <span className="name">SpclassNameerMan</span>
+                    <span className="delete">delete</span>
+                </li>
+            </ul>
+        </div>
+        <form className="add-book">
+            <input type="text" placeholder="Add a movie..."/>
+            <button>Add</button>
+        </form>
+    </div>
     </>
   )
 }
